769c392b9571a8277ab253d5585a70144e17fe37
eric-elem <eric.elem@andela.com>
2017-09-05 07:38:00 +0300

    ch(test_check_token): Add tests
    Tests for already logged in user, bad token, bad username

--- hc/accounts/tests/test_check_token.py
+++ hc/accounts/tests/test_check_token.py
@@ -22,7 +22,22 @@ class CheckTokenTestCase(BaseTestCase):
         self.assertEqual(self.profile.token, "")
 
     ### Login and test it redirects already logged in
+    def test_redirects_already_logged_in_user(self):
+        # login user alice the first time
+        self.client.post("/accounts/check_token/alice/secret-token/")
 
-    ### Login with a bad token and check that it redirects
+        # login user alice the second time redirects
+        response = self.client.get("/accounts/check_token/alice/secret-token/")
+        self.assertRedirects(response, "/checks/")
 
+    ### Login with a bad token redirects to login page
+    def test_redirects_with_bad_token(self):
+        response = self.client.post("/accounts/check_token/alice/bad-token/")
+        self.assertRedirects(response, '/accounts/login/')
+    
     ### Any other tests?
+
+    ### Login with a bad username redirects to login page
+    def test_redirects_with_bad_username(self):
+        response = self.client.post("/accounts/check_token/tom/secret-token/")
+        self.assertRedirects(response, '/accounts/login/')
