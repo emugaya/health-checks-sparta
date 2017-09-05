805b326712e3ba266b88c886310064753ff26198
patlub <patricklu2010@gmail.com>
2017-09-05 11:06:01 +0300

    Implement modal for blog table

--- hc/front/models.py
+++ hc/front/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    category = models.TextField()
+    story = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    user = models.TextField()
+
+
+    def __str__(self):
+        return self.title
