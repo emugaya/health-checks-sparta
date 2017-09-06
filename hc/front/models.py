9a1e485604430189fc3da037e40eba6f84fc8e7b
Lemmah <lemayiannakolah@gmail.com>
2017-09-06 07:17:58 +0300

    feat(FAQs): Create a Model for QnA
    - FAQs are mainly QnAs, so a model of just QnA will just do.
    - QnA -> Questions and Answers

--- hc/front/models.py
+++ hc/front/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class FAQnAnswers(models.Model):
+    """ Model for FAQs """
+    question = models.CharField(max_length=200)
+    answer = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+
