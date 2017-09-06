9872af9f9dabe63584a87ce81d67c752206f0854
eric-elem <eric.elem@andela.com>
2017-09-06 14:42:50 +0300

    ch(test_team_access_middleware): Refactor and add comments

--- hc/accounts/tests/test_team_access_middleware.py
+++ hc/accounts/tests/test_team_access_middleware.py
@@ -1,18 +1,21 @@
+""" Holds tests form the team access middleware """
+
 from django.contrib.auth.models import User
 from django.test import TestCase
 from hc.accounts.models import Profile
 
 
 class TeamAccessMiddlewareTestCase(TestCase):
+    """ Tests the middleware for Team Access """
 
     def test_it_handles_missing_profile(self):
+        """ Creates a user ned, does not assign a profile, logs in and
+        checks that the middleware creates a profile for the user when
+        they try to visit any page """
+
         user = User(username="ned", email="ned@example.org")
         user.set_password("password")
         user.save()
-
         self.client.login(username="ned@example.org", password="password")
-        r = self.client.get("/about/")
-        self.assertEqual(r.status_code, 200)
-
-        ### Assert the new Profile objects count
+        self.client.get("/about/")
         self.assertEqual(Profile.objects.all().count(), 1)
