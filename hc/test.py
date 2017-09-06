abb687393f5b103585c407c663db2c60527092a6
eric-elem <eric.elem@andela.com>
2017-09-06 15:43:43 +0300

    ch(test): Refactor and add comments

--- hc/test.py
+++ hc/test.py
@@ -1,3 +1,5 @@
+""" Defines a base test for all other tests """
+
 from django.contrib.auth.models import User
 from django.test import TestCase
 
@@ -5,34 +7,29 @@ from hc.accounts.models import Member, Profile
 
 
 class BaseTestCase(TestCase):
+    """ Sets up parameters required by all tests """
 
     def setUp(self):
-        super(BaseTestCase, self).setUp()
+        """ Calls super's setUp, Creates three users; alice with team
+        access enabled, bob on alice's team, and charlie. """
 
-        # Alice is a normal user for tests. Alice has team access enabled.
+        super(BaseTestCase, self).setUp()
         self.alice = User(username="alice", email="alice@example.org")
         self.alice.set_password("password")
         self.alice.save()
-
         self.profile = Profile(user=self.alice, api_key="abc")
         self.profile.team_access_allowed = True
         self.profile.save()
 
-        # Bob is on Alice's team and should have access to her stuff
         self.bob = User(username="bob", email="bob@example.org")
         self.bob.set_password("password")
         self.bob.save()
-
         self.bobs_profile = Profile(user=self.bob)
         self.bobs_profile.current_team = self.profile
         self.bobs_profile.save()
+        member = Member(team=self.profile, user=self.bob)
+        member.save()
 
-        m = Member(team=self.profile, user=self.bob)
-        m.save()
-
-        # Charlie should have no access to Alice's stuff
         self.charlie = User(username="charlie", email="charlie@example.org")
         self.charlie.set_password("password")
         self.charlie.save()
-
-        ### Set Charles not to have access to Alice's stuff
