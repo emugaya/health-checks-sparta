8afcd015aeb393864d8d719b952630357f4e06ab
patlub <patricklu2010@gmail.com>
2017-09-07 22:32:39 +0300

    ft(test_delete_blogs_posts.py) Add test for deleting a blog post

--- hc/front/tests/test_delete_blogs_posts.py
+++ hc/front/tests/test_delete_blogs_posts.py
@@ -5,9 +5,9 @@ from hc.test import BaseTestCase
 
 
 @override_settings(PUSHOVER_API_TOKEN="token", PUSHOVER_SUBSCRIPTION_URL="url")
-class GetBlogsTestCase(BaseTestCase):
+class DeleteBlogsTestCase(BaseTestCase):
     def setUp(self):
-        super(GetBlogsTestCase, self).setUp()
+        super(DeleteBlogsTestCase, self).setUp()
         url = "/blog/add/"
         form = {"blog_title": "Blog Post Title",
                 "category": "Technology", "story": "This is a story"}
@@ -19,9 +19,15 @@ class GetBlogsTestCase(BaseTestCase):
         self.client.post(url, form)
         self.client.post(url, form2)
 
-    def tests_gets_blog_posts(self):
-        url = "/blog/"
-        assert Blog.objects.count() == 2
-        r = self.client.get(url)
-        self.assertContains(r, "Blog Post Title")
-        self.assertContains(r, "Blog Post Title 2")
+    def tests_deletes_blog_posts(self):
+        # Delete first blog post
+        id = Blog.objects.all()[0].id
+        url = "/blog/delete/%s/" % id
+        self.client.get(url)
+        assert Blog.objects.count() == 1
+
+        # Delete second blog post
+        id = Blog.objects.all()[0].id
+        url = "/blog/delete/%s/" % id
+        self.client.get(url)
+        assert Blog.objects.count() == 0
