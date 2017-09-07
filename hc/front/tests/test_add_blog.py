eba923c18d760093939340f0d6041f1cb3fe5e10
patlub <patricklu2010@gmail.com>
2017-09-07 16:54:14 +0300

    ft(test_add_blog.py) Add tests for adding a blog

---
+++ hc/front/tests/test_add_blog.py
@@ -0,0 +1,26 @@
+
+from django.test.utils import override_settings
+
+from hc.front.models import Blog
+from hc.test import BaseTestCase
+
+@override_settings(PUSHOVER_API_TOKEN="token", PUSHOVER_SUBSCRIPTION_URL="url")
+class AddBlogTestCase(BaseTestCase):
+    def test_it_adds_blog_post(self):
+        url = "/blog/add/"
+        form = {"blog_title": "Blog Post Title", "category": "Technology", "story": "This is a story"}
+
+        self.client.login(username="alice@example.org", password="password")
+        r = self.client.post(url, form)
+
+        self.assertRedirects(r, "/blog/")
+        assert Blog.objects.count() == 1
+
+    def test_add_blog_with_required_fields_empty(self):
+        url = "/blog/add/"
+        form = {"blog_title": "Blog Post Title", "category": "Technology", "story": ""}
+
+        self.client.login(username="alice@example.org", password="password")
+        r = self.client.post(url, form)
+
+        assert Blog.objects.count() == 0
