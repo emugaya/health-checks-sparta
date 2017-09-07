0d8780f5cf01d2b92e5c74204460783fccb5c5f1
Lemmah <lemayiannakolah@gmail.com>
2017-09-07 07:27:19 +0300

    feat(FAQs): Make Migrations for FAQsnAs
    - FAQsnAs -> Frequently Asked Questions and Answers

--- hc/front/models.py
+++ hc/front/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
 class FAQnAnswers(models.Model):
     """ Model for FAQs """
     question = models.CharField(max_length=200)
