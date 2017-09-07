866e2ed151dfaf37ad7ec953fd8a6a677ebae412
patlub <patricklu2010@gmail.com>
2017-09-07 16:56:55 +0300

    ft(test_add_blog.py) Refactor and add docstrings

--- hc/front/tests/test_add_blog.py
+++ hc/front/tests/test_add_blog.py
@@ -6,9 +6,11 @@ from hc.test import BaseTestCase
 
 @override_settings(PUSHOVER_API_TOKEN="token", PUSHOVER_SUBSCRIPTION_URL="url")
 class AddBlogTestCase(BaseTestCase):
+    "Should test if blog is added"
     def test_it_adds_blog_post(self):
         url = "/blog/add/"
-        form = {"blog_title": "Blog Post Title", "category": "Technology", "story": "This is a story"}
+        form = {"blog_title": "Blog Post Title",
+                "category": "Technology", "story": "This is a story"}
 
         self.client.login(username="alice@example.org", password="password")
         r = self.client.post(url, form)
@@ -17,8 +19,10 @@ class AddBlogTestCase(BaseTestCase):
         assert Blog.objects.count() == 1
 
     def test_add_blog_with_required_fields_empty(self):
+        "Should test that blog is not added when some fields are empty"
         url = "/blog/add/"
-        form = {"blog_title": "Blog Post Title", "category": "Technology", "story": ""}
+        form = {"blog_title": "Blog Post Title"
+            , "category": "Technology", "story": ""}
 
         self.client.login(username="alice@example.org", password="password")
         r = self.client.post(url, form)
