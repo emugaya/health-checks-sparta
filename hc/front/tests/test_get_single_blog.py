690c3fb74c0ab414b23a95e7aeea7eea610bfef0
patlub <patricklu2010@gmail.com>
2017-09-07 19:15:46 +0300

    ft(test_get_single_blog.py) test viewing or fetching single blog post

--- hc/front/tests/test_get_single_blog.py
+++ hc/front/tests/test_get_single_blog.py
@@ -1,30 +1,28 @@
-
 from django.test.utils import override_settings
 
 from hc.front.models import Blog
 from hc.test import BaseTestCase
 
+
 @override_settings(PUSHOVER_API_TOKEN="token", PUSHOVER_SUBSCRIPTION_URL="url")
-class AddBlogTestCase(BaseTestCase):
-    "Should test if blog is added"
-    def test_it_adds_blog_post(self):
+class GetSingleBlogTestCase(BaseTestCase):
+    def setUp(self):
+        super(GetSingleBlogTestCase, self).setUp()
         url = "/blog/add/"
         form = {"blog_title": "Blog Post Title",
                 "category": "Technology", "story": "This is a story"}
 
         self.client.login(username="alice@example.org", password="password")
-        r = self.client.post(url, form)
+        self.client.post(url, form)
 
-        self.assertRedirects(r, "/blog/")
-        assert Blog.objects.count() == 1
+    def test_it_gets_a_single_blog_post(self):
+        id = Blog.objects.all()[0].id
+        url = "/blog/%s" % id
+        r = self.client.get(url)
+        self.assertContains(r, "Blog Post Title")
 
-    def test_add_blog_with_required_fields_empty(self):
-        "Should test that blog is not added when some fields are empty"
-        url = "/blog/add/"
-        form = {"blog_title": "Blog Post Title"
-            , "category": "Technology", "story": ""}
-
-        self.client.login(username="alice@example.org", password="password")
-        r = self.client.post(url, form)
-
-        assert Blog.objects.count() == 0
+    def test_when_bucket_id_doent_exist(self):
+        id = Blog.objects.all()[0].id + 1
+        url = "/blog/%s" % id
+        r = self.client.get(url)
+        self.assertContains(r, "Page Not Found")
