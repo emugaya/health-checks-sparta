612f33ef5bb273ab107031f3618f77ba2cab55f8
eric-elem <eric.elem@andela.com>
2017-09-14 08:16:33 +0300

    ft(front.urls): Add priority and escalation urls

--- hc/front/urls.py
+++ hc/front/urls.py
@@ -4,6 +4,8 @@ from hc.front import views
 
 check_urls = [
     url(r'^name/$', views.update_name, name="hc-update-name"),
+    url(r'^priority/$', views.update_priority, name="hc-update-priority"),
+    url(r'^escalations/$', views.escalation_matrix, name="hc-escalation-matrix"),
     url(r'^timeout/$', views.update_timeout, name="hc-update-timeout"),
     url(r'^pause/$', views.pause, name="hc-pause"),
     url(r'^remove/$', views.remove_check, name="hc-remove-check"),
