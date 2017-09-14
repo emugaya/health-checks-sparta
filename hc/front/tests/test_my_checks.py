ea4280122b4b8a600ee7bb32cc07370569f815a8
eric-elem <eric.elem@andela.com>
2017-09-14 08:13:23 +0300

    ft(test_my_checks): Add tests and refractor
    Tests for setting priority, enabling and disabling escalations

--- hc/front/tests/test_my_checks.py
+++ hc/front/tests/test_my_checks.py
@@ -6,8 +6,14 @@ from django.utils import timezone
 
 class MyChecksTestCase(BaseTestCase):
 
+    post_data = {
+        "interval": 3600,
+        "emails": "test1@mail.com; test2@mail.com"
+    }
+
     def setUp(self):
         super(MyChecksTestCase, self).setUp()
+        self.client.login(username="alice@example.org", password="password")
         self.check = Check(user=self.alice, name="Alice Was Here")
         self.check.save()
 
@@ -29,7 +35,7 @@ class MyChecksTestCase(BaseTestCase):
         self.check.status = "up"
         self.check.save()
 
-        self.client.login(username="alice@example.org", password="password")
+        
         r = self.client.get("/checks/")
 
         # Desktop
@@ -43,7 +49,7 @@ class MyChecksTestCase(BaseTestCase):
         self.check.status = "up"
         self.check.save()
 
-        self.client.login(username="alice@example.org", password="password")
+        
         r = self.client.get("/checks/")
 
         # Desktop
@@ -57,7 +63,7 @@ class MyChecksTestCase(BaseTestCase):
         self.check.status = "up"
         self.check.save()
 
-        self.client.login(username="alice@example.org", password="password")
+        
         r = self.client.get("/checks/")
 
         # Desktop
@@ -65,3 +71,44 @@ class MyChecksTestCase(BaseTestCase):
 
         # Mobile
         self.assertContains(r, "label-warning")
+
+    def test_owner_can_update_priority(self):
+        
+        self.post_priority()
+        self.check.refresh_from_db()
+        self.assertEqual(self.check.priority, 3)
+
+    def test_teammember_can_update_priority(self):
+        self.client.login(username="bob@example.org", password="password")
+        self.post_priority()
+        self.check.refresh_from_db()
+        self.assertEqual(self.check.priority, 3)
+
+    def test_nonteammember_cannot_update_priorityr(self):
+        self.client.login(username="charlie@example.org", password="password")
+        response = self.post_priority()
+        self.assertEqual(response.status_code, 403)
+        self.check.refresh_from_db()
+        self.assertNotEqual(self.check.priority, 3)
+
+    def test_can_enable_escalation(self):
+
+        self.post_data["enabled"] = True
+        self.client.post('/checks/' + str(self.check.code) + '/escalations/',
+                         self.post_data)
+        self.check.refresh_from_db()
+        self.assertTrue(self.check.escalation_enabled)
+        self.assertEqual(self.check.escalation_interval, td(seconds=3600))
+        self.assertEqual(self.check.escalation_list, self.post_data['emails'])
+
+    def test_can_disable_escalation(self):
+        
+        self.post_data["enabled"] = False
+        self.client.post('/checks/' + str(self.check.code) + '/escalations/',
+                         self.post_data)
+        self.check.refresh_from_db()
+        self.assertFalse(self.check.escalation_enabled)
+
+    def post_priority(self):
+        return self.client.post('/checks/' + str(self.check.code) + '/priority/',
+                                { "priority": 3 })
